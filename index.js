d28a0fa7fc382662390372e4ce8d5e81fccc11ca
omerh <haimomer@gmail.com>
2016-06-02 11:51:45 +0300

    kk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hoello World!');
+  res.send('Hooello World!');
 });
 
 app.listen(3000, function () {
